241ce870a1edc8af01e23a6b90e94dbba3439328
yan <yan@yandeMacBook-Air-2.local>
2020-07-03 22:57:35 +0800

    引入antdesin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button type="primary">
+      Primary
+    </Button>
   </div>
 </template>
+<script>
+import { Button } from 'ant-design-vue'
+export default {
+  name: 'about',
+  components: {
+    Button
+  }
+}
+</script>
