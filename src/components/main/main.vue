a14568caa147931a788ddf1289b2fcda0d6b675a
yan <yan@yandeMacBook-Air-2.local>
2020-07-04 19:18:18 +0800

    基础文件结构添加config、router、vuex

---
+++ src/components/main/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <layout></layout>
+</template>
+<script>
+import Layout from 'ant-design-vue'
+export default {
+    name: 'mian',
+    components: {
+        Layout
+    },
+    mounted () {
+
+    },
+    methods: {
+
+    }
+}
+</script>
