c8739ab826e88b16630419f53c568458ea8167ad
yan <yan@yandeMacBook-Air-2.local>
2020-07-12 15:49:41 +0800

    ...

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,22 @@ import Home from '../views/Home.vue'
 import Main from '../components/main'
 const routes = [
     {
+      path: '/login',
+      name: 'login',
+      meta: {
+        title: '登录'
+      },
+      component: () => import(/* webpackChunkName: "login" */ '../views/login')
+    },
+    {
       path: '/',
-      name: 'Home',
+      name: '_home',
       redirect: '/home',
       component: Main,
       children: [
           {
               path: '/home',
-              name: '_Home',
+              name: 'home',
               component: Home
           }
       ]
@@ -21,11 +29,6 @@ const routes = [
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import(/* webpackChunkName: "login" */ '../views/login')
     }
 ]
 const authority = [
