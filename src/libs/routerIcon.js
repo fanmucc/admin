728e51a86e45cdfd35765042218fab57496b96df
fanmu <fanmucc@yeah.net>
2020-07-13 17:53:35 +0800

    渲染icon

---
+++ src/libs/routerIcon.js
@@ -0,0 +1,13 @@
+/**
+ * 动态渲染侧边栏icon
+ */
+
+ export const routeIcon = (name) => {
+    switch (name) {
+        case 'home':
+            return ''
+            break;
+        default: 
+            return ''
+    }
+ }
